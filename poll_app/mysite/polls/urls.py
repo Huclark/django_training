691c25f1154b28eb544192ef58f3ca178866a675
Huclark <vhuclark@gmail.com>
2024-05-14 10:17:41 +0000

    Add: project

---
+++ poll_app/mysite/polls/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+# name the app so django can distinguish views of more than
+# one app which have the same name
+app_name = "polls"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
