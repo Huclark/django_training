b8822f26fcdb0ac50c37edfa5f93a1fb46c77494
Huclark <vhuclark@gmail.com>
2024-05-20 07:37:26 +0000

    latest

--- poll_app/mysite/polls/views.py
+++ poll_app/mysite/polls/views.py
@@ -3,10 +3,29 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+from .serializers import GroupSerializer, UserSerializer
+
 from .models import Question, Choice
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
